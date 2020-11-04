4092c285c0b3c8552ceeac3ac4cbb2340b9234d0
Toyin Adesina <toyinadesina60@gmail.com>
2020-11-05 00:41:45 +0100

    version 1.0.2

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
   $(".home_bg").vegas({
     slides: [
         { src: "/images/display_img/church.jpg" },
+        { src: "/images/display_img/picp1.jpeg" },
         { src: "/images/display_img/church.jpeg" },
         { src: "/images/display_img/church2.jpg" },
+        { src: "/images/display_img/picp2.jpeg" },
         { src: "/images/display_img/church3.jpg" },
         { src: "/images/display_img/church4.jpg" },
+        { src: "/images/display_img/picp3.jpeg" },
     ]
   });
 })
